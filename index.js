e01b5f4222dc6212d9f51a46e553735660da464c
gnehs <you@example.com>
2018-10-04 19:20:36 +0800

    支援 otpauth 格式

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const notp = require('notp'),
     fs = require('fs'),
-    base32 = require('thirty-two'),
     jsonfile = require('jsonfile'),
-    schedule = require('node-schedule')
+    schedule = require('node-schedule'),
+    base32 = require('thirty-two')
 if (!fs.existsSync('./config.json')) {
     console.error('no config.json')
     process.exit()
@@ -26,17 +26,18 @@ bot.onText(/\/start/, (msg) => {
 bot.onText(/\/add/, (msg) => {
     status[msg.chat.id] = {}
     status[msg.chat.id].status = "getSecret"
-    bot.sendMessage(msg.chat.id, `輸入您的 secret 或是送出一張 QRCode`, { reply_to_message_id: msg.message_id });
+    bot.sendMessage(msg.chat.id, `輸入您的 secret 或是送出一張 otpauth:// 格式的連結`, { reply_to_message_id: msg.message_id });
 });
 bot.onText(/\/cencel/, (msg) => {
     status[msg.chat.id].status = false
     bot.sendMessage(msg.chat.id, `已取消`, { reply_to_message_id: msg.message_id });
 });
 bot.onText(/\/get/, msg => {
-    let resp = `驗證碼：\n`
+    let resp = `<b>驗證碼</b>\n`
     for (let i in data[msg.chat.id].secret) {
         let name = data[msg.chat.id].secret[i].name,
             key = notp.totp.gen(data[msg.chat.id].secret[i].secret)
+            //rekey = key.replace(/\D/g, '').replace(/...(?!$)/g, '$& ') //插入空格
         resp += `<code>${key}</code> (${name})\n`
     }
     resp = data[msg.chat.id].secret.length > 0 ? resp : '尼沒新增驗證碼要拿什麼ㄋ\n使用 /add 新增驗證碼'
@@ -74,7 +75,7 @@ bot.on('callback_query', callbackQuery => {
         });
     }
 });
-bot.on('message', (msg) => {
+bot.on('message', async(msg) => {
     if (!data[msg.chat.id])
         data[msg.chat.id] = { "secret": [] }
     if (status[msg.chat.id]) {
@@ -82,9 +83,29 @@ bot.on('message', (msg) => {
         let status = userData.status
         switch (status) {
             case "getSecret":
-                userStatus(msg.chat.id, { status: "setName", data: msg.text })
-                let nowkey = notp.totp.gen(msg.text)
-                bot.sendMessage(msg.chat.id, `目前的驗證碼是：<code>${nowkey}</code>\n請輸入一個名稱：`, { parse_mode: "HTML", reply_to_message_id: msg.message_id });
+                let otpauth = /otpauth:\/\/totp\/(.+).secret=(.+)\&|otpauth:\/\/totp\/(.+).secret=(.+)/
+                if (msg.text.match(otpauth)) {
+                    let resp = ``
+                    try {
+                        let key = msg.text.match(otpauth)[2];
+                        let name = msg.text.match(otpauth)[1];
+                        name = name.match(":") ? name.replace(/:/g, ' /') : name
+                        key = base32.decode(key)
+                        let nowkey = notp.totp.gen(key)
+                        data[msg.chat.id].secret.push({
+                            "name": name,
+                            "secret": key
+                        })
+                        resp = `加入成功！\n驗證碼：<code>${nowkey}</code>\n名稱：<code>${name}</code>`
+                    } catch (e) {
+                        resp = `發生了錯誤：（`
+                    }
+                    bot.sendMessage(msg.chat.id, resp, { parse_mode: "HTML", reply_to_message_id: msg.message_id });
+                } else {
+                    userStatus(msg.chat.id, { status: "setName", data: msg.text })
+                    let nowkey = notp.totp.gen(msg.text)
+                    bot.sendMessage(msg.chat.id, `目前的驗證碼是：<code>${nowkey}</code>\n請輸入一個名稱：`, { parse_mode: "HTML", reply_to_message_id: msg.message_id });
+                }
                 break;
             case "setName":
                 userStatus(msg.chat.id, { status: false })
